57c97a3f736d1902c6b7b44bd0be8c138afcdbc2
AndersonSantosFernandes <andersantfer@gmail.com>
2022-11-08 19:58:13 -0300

    Separando templates

---
+++ 19_MOVIESTAR/templates/header.php
@@ -0,0 +1,46 @@
+<?php
+require_once("globals.php");
+include_once("db.php");
+?>
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieStar</title>
+    <link rel="short icon" href="<?=$BASE_URL?>img/moviestar.ico" >
+    <!--Link bootstrap-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Link font awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Css do projeto-->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>   
+   <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+        <a href="<?=$BASE_URL?>" class="navbar-brand">
+            <img src="<?=$BASE_URL?>img/logo.svg" alt="MovieStar" id="logo">
+            <span id="moviestar-title">MovieStar</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggler="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle
+        navigation">
+           <i class="fas fa-bar"></i> 
+        </button>
+        <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+            <input type="text" name="q" id="search" class="form-control mr-sm-2"
+            type="search" placeholder="Buscar filmes" aria-label="Search">
+            <button class="btn my-2 mysm-0" type="submit">
+                <i class="fas fa-search"></i>
+            </button>
+        </form>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="<?=$BASE_URL?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </header> 
