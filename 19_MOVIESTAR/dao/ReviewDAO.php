deadee508ad79e5055bf3570101f0f8c02fefc8b
AndersonSantosFernandes <andersantfer@gmail.com>
2022-12-24 18:27:00 -0300

    Buscando dados do usuário

--- 19_MOVIESTAR/dao/ReviewDAO.php
+++ 19_MOVIESTAR/dao/ReviewDAO.php
@@ -52,8 +52,20 @@ class ReviewDAO implements ReviewDAOInterface{
         if($stmt->rowCount() > 0){
             $reviewsData = $stmt->fetchAll();
 
+            $userDao = new UserDAO($this->conn, $this->url);
+
             foreach($reviewsData as $review){
-                $reviews[] = $this->buildReview($review);
+
+                $reviewObject = $this->buildReview($review);
+                
+                //Chamar os dados do usuário
+                $user = $userDao->findById($reviewObject->users_id);
+
+                $reviewObject->user = $user;
+
+                $reviews[] = $reviewObject;
+
+
             }
         }
         return $reviews;
--- 19_MOVIESTAR/templates/user_review.php
+++ 19_MOVIESTAR/templates/user_review.php
@@ -1,12 +1,28 @@
+<?php
+
+require_once("models/User.php");
+
+$userModel = new User();
+
+$fullName = $userModel->getFulName($review->user);
+
+//Checar se o usuário tem imagem
+if ($review->user->image == "") {
+    $review->user->image = "user1.png";
+  }
+
+
+?>
+
 <div class="col-md-12 review">
     <div class="row">
         <div class="col-md-1">
             <div class="profile-image-container review-image"
-                style="background-image: url('<?= $BASE_URL ?>img/users/user1.png')"></div>
+                style="background-image: url('<?= $BASE_URL ?>img/users/<?= $review->user->image ?>')"></div>
         </div>
         <div class="col-md-9 author-details-container">
             <h4 class="author-name">
-                <a href="#">Testando</a>
+                <a href="#"><?= $fullName ?></a>
             </h4>
             <p><i class="fas fa-star"></i><?= $review->rating ?></p>
         </div>
